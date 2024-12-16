643787d43fead129cee7da5f0257c4ea5309a7a6
AndresSanchezUPM <upmandres@gmail.com>
2024-12-16 23:11:58 +0100

    Ajustes para pruebas del sensor de pulso

--- Robot/Arduino/graficarPulso/graficarPulso.ino
+++ Robot/Arduino/graficarPulso/graficarPulso.ino
@@ -4,14 +4,11 @@ void setup() {
 }
 
 void loop() {
-  // Lee el valor del pin A0 (valor entre 0 y 1023)
-  const int maxValue=600;
-  const int minValue=420;
-  int sensorValue = constrain(analogRead(A0),minValue,maxValue);
-  float processedValue = (sensorValue-minValue)/float(maxValue-minValue);
+  int sensorValue = analogRead(A0);
+  //float processedValue = (sensorValue-minValue)/float(maxValue-minValue);
 
   // Envía el valor leído al monitor serial
-  Serial.println(processedValue*100);
+  Serial.println(sensorValue);
 
   // Espera un breve tiempo antes de leer el valor de nuevo
   delay(40);  // Retardo de 100 ms (ajustable)
