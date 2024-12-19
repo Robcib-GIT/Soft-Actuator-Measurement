096d4cf56c5fe495a461e9ea7a1a8e6274df45d8
AndresSanchezUPM <upmandres@gmail.com>
2024-12-19 13:46:24 +0100

    Detectar sistolicos y diastolicos

--- Otros/Pruebas/Python/DeteccionPulso/main.py
+++ Otros/Pruebas/Python/DeteccionPulso/main.py
@@ -3,6 +3,11 @@ from scipy.signal import butter, lfilter
 import numpy as np
 from scipy.signal import find_peaks
 
+# TODO ajustar
+MAX_SISTOLICO = 550
+BARRERA_SIS_DIA = 515
+MIN_DIASTOLICO = 480
+MIN_MUESTRAS_ENTRE_PULSOS = (60/100)*(1000/40)
 
 def leer_datos(archivo):
     with open(archivo, 'r') as f:
@@ -42,7 +47,7 @@ def aplicar_deteccion_PPG(lista_detecciones):
 
 if __name__ == '__main__':
     # ##################### OBTENER DATOS ####################################
-    ruta_datos = "../Data/SalidaPulsoSujeto1.txt"
+    ruta_datos = "../Data/SalidaPulsoSujeto1_RUIDO.txt"
     datos_pulso = leer_datos(ruta_datos)
     dt = 40
     tiempo = [dt * i for i in range(len(datos_pulso))]
@@ -60,6 +65,14 @@ if __name__ == '__main__':
         senal_filtrada.append(y[0])
 
     indices_picos, _ = find_peaks(senal_filtrada, distance=2)
+    indices_sistolicos, _ = find_peaks(
+        senal_filtrada,
+        height=(BARRERA_SIS_DIA, MAX_SISTOLICO),
+        distance=MIN_MUESTRAS_ENTRE_PULSOS)
+    indices_diastolicos, _ = find_peaks(
+        senal_filtrada,
+        height=(MIN_DIASTOLICO, BARRERA_SIS_DIA),
+        distance=MIN_MUESTRAS_ENTRE_PULSOS)
     maximos_tiempo = [[], []]
     sistolicos_tiempo = [[], []]
     diastolicos_tiempo = [[], []]
@@ -67,7 +80,15 @@ if __name__ == '__main__':
     for indice in indices_picos:
         maximos_tiempo[0].append(senal_filtrada[indice])
         maximos_tiempo[1].append(tiempo[indice])
-        # TODO filtrar y añadir picos sistólicos y diastólicos
+
+    # TODO filtrar y añadir picos sistólicos y diastólicos
+    for indice in indices_sistolicos:
+        sistolicos_tiempo[0].append(senal_filtrada[indice])
+        sistolicos_tiempo[1].append(tiempo[indice])
+
+    for indice in indices_diastolicos:
+        diastolicos_tiempo[0].append(senal_filtrada[indice])
+        diastolicos_tiempo[1].append(tiempo[indice])
 
     # ##################### Detección de señal PPG ####################################
     PPG_detectado = []
@@ -85,22 +106,41 @@ if __name__ == '__main__':
     axs[0].plot(
         tiempo,
         datos_pulso,
+        color='blue',
         linestyle='-',
         linewidth=0.5,
         label="Onda PPG original")
     axs[0].plot(
         tiempo,
         senal_filtrada,
+        color='green',
         linestyle='-',
         linewidth=1.5,
         label="Onda PPG filtrada")
+
     axs[0].scatter(
-        maximos_tiempo[1],
-        maximos_tiempo[0],
+        sistolicos_tiempo[1],
+        sistolicos_tiempo[0],
+        marker='o',
+        color='blue',
+        s=15,
+        label='Picos sistólicos')
+
+    axs[0].scatter(
+        diastolicos_tiempo[1],
+        diastolicos_tiempo[0],
+        marker='s',
+        color='red',
+        s=12,
+        label='Picos diastólicos')
+    '''
+    axs[0].scatter(
+        sistolicos_tiempo[1],
+        sistolicos_tiempo[0],
         color='blue',
         s=15,
         label='Máximos')
-
+    '''
     axs[0].set_xlabel('Tiempo [ms]')
     axs[0].set_ylim(min(datos_pulso) - 10, max(datos_pulso) + 10)
     axs[0].set_ylabel('Amplitud [mV]')
