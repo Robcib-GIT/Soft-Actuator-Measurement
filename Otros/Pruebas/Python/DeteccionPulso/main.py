dd15583b6722b142dd2ba22e5496239e691af37b
AndresSanchezUPM <upmandres@gmail.com>
2024-12-19 12:24:40 +0100

    Detectar cuando hay pulso

--- Otros/Pruebas/Python/DeteccionPulso/main.py
+++ Otros/Pruebas/Python/DeteccionPulso/main.py
@@ -25,15 +25,14 @@ def aplicar_filtro_paso_bajo(data, b, a, zi=None):
 
 
 if __name__ == '__main__':
-    ruta_datos = "../Data/SalidaPulsoSujeto1_v3.txt"
+    ruta_datos = "../Data/SalidaPulsoSujeto1_RUIDO.txt"
     datos_pulso = leer_datos(ruta_datos)
     dt = 40
-    tiempo = [dt*i for i in range(len(datos_pulso))]
+    tiempo = [dt * i for i in range(len(datos_pulso))]
 
-    lenD = len(datos_pulso)
-    lenT = len(tiempo)
+    # ##################### FILTRAR SEÑAL ####################################
 
-    fs = 1000/dt  # Frecuencia de muestreo (Hz)
+    fs = 1000 / dt  # Frecuencia de muestreo (Hz)
     cutoff = 3  # Frecuencia de corte (Hz) (entre 2 y 5 va bien)
     b, a = init_filtro_paso_bajo(cutoff, fs)
     zi = None
@@ -57,19 +56,64 @@ if __name__ == '__main__':
         + filtrar por rango -> si se cumplen las 2 activar pulso_detectado
         """
 
-    plt.figure(figsize=(13, 6))
-    plt.plot(tiempo, datos_pulso, linestyle='-', linewidth=0.5, label="Onda PPG")
-    plt.plot(tiempo, senal_filtrada, linestyle='-', linewidth=1.5, label="Onda PPG filtrada")
-    plt.scatter(
+    # ##################### Detección de señal PPG ####################################
+    PPG_detectado = []
+    detectado = False
+    for i in range(len(senal_filtrada)):
+        if 560 > senal_filtrada[i] > 480:
+            if i in indices_picos:
+                detectado = True
+        else:
+            detectado = False
+        PPG_detectado.append(detectado)
+
+    # ##################### GRAFICAR RESULTADOS ####################################
+    fig, axs = plt.subplots(2, 1, figsize=(13, 6))
+
+    axs[0].axhline(
+        y=1023 / 2,
+        color='black',
+        linestyle='--',
+        linewidth=1,
+        alpha=0.6)
+    axs[0].plot(
+        tiempo,
+        datos_pulso,
+        linestyle='-',
+        linewidth=0.5,
+        label="Onda PPG original")
+    axs[0].plot(
+        tiempo,
+        senal_filtrada,
+        linestyle='-',
+        linewidth=1.5,
+        label="Onda PPG filtrada")
+    axs[0].scatter(
         maximos_tiempo[1],
         maximos_tiempo[0],
         color='blue',
         s=15,
         label='Máximos')
-    plt.xlim(min(tiempo), max(tiempo))
-    plt.ylim(min(datos_pulso) - 10, max(datos_pulso) + 10)
-    plt.xlabel('Tiempo [ms]')
-    plt.ylabel('Amplitud [mV]')
-    plt.title('Detección de onda PPG')
-    plt.legend()
+
+    axs[0].set_xlabel('Tiempo [ms]')
+    axs[0].set_ylim(min(datos_pulso) - 10, max(datos_pulso) + 10)
+    axs[0].set_ylabel('Amplitud [mV]')
+    axs[0].set_title('Detección de onda PPG')
+    # axs[0].legend()
+
+    axs[1].plot(
+        tiempo,
+        PPG_detectado,
+        linestyle='-',
+        linewidth=1.5,
+        label="PPG detectada")
+    axs[1].set_xlabel("Tiempo [ms]")
+    axs[1].autoscale(axis='y')
+    axs[1].set_ylabel("PPG detectada [0-1]")
+    axs[1].set_title("Detección de onda PPG")
+
+    for ax in axs:
+        ax.autoscale(axis='x')
+        ax.set_xmargin(0)
+    plt.tight_layout()
     plt.show()
