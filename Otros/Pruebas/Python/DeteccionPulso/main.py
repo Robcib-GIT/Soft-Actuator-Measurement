876db5a0187500709230cb67ad4fe10e066d10e0
AndresSanchezUPM <upmandres@gmail.com>
2024-12-19 02:50:03 +0100

    Encontrar maximos

--- Otros/Pruebas/Python/DeteccionPulso/main.py
+++ Otros/Pruebas/Python/DeteccionPulso/main.py
@@ -1,6 +1,7 @@
 from matplotlib import pyplot as plt
 from scipy.signal import butter, lfilter
 import numpy as np
+from scipy.signal import find_peaks
 
 
 def leer_datos(archivo):
@@ -42,9 +43,21 @@ if __name__ == '__main__':
         y, zi = aplicar_filtro_paso_bajo([dato], b, a, zi)
         senal_filtrada.append(y[0])
 
+    indices_picos, _ = find_peaks(senal_filtrada, distance=2)
+    maximos_tiempo = [[], []]
+    for indice in indices_picos:
+        maximos_tiempo[0].append(senal_filtrada[indice])
+        maximos_tiempo[1].append(tiempo[indice])
+
     plt.figure(figsize=(13, 6))
     plt.plot(tiempo, datos_pulso, linestyle='-', linewidth=0.5, label="Onda PPG")
     plt.plot(tiempo, senal_filtrada, linestyle='-', linewidth=1.5, label="Onda PPG filtrada")
+    plt.scatter(
+        maximos_tiempo[1],
+        maximos_tiempo[0],
+        color='blue',
+        s=15,
+        label='Máximos')
     plt.xlim(min(tiempo), max(tiempo))
     plt.ylim(min(datos_pulso) - 10, max(datos_pulso) + 10)
     plt.xlabel('Tiempo [ms]')
