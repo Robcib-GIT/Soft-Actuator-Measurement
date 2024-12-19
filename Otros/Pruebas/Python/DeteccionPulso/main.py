cc8c04bd7aa7821bfdd90997c9c74bfa275b03fe
AndresSanchezUPM <upmandres@gmail.com>
2024-12-19 03:05:25 +0100

    Encontrar maximos

--- Otros/Pruebas/Python/DeteccionPulso/main.py
+++ Otros/Pruebas/Python/DeteccionPulso/main.py
@@ -25,16 +25,16 @@ def aplicar_filtro_paso_bajo(data, b, a, zi=None):
 
 
 if __name__ == '__main__':
-    ruta_datos = "../Data/SalidaPulsoSujeto2.txt"
+    ruta_datos = "../Data/SalidaPulsoSujeto1_v3.txt"
     datos_pulso = leer_datos(ruta_datos)
     dt = 40
-    tiempo = [40*i for i in range(len(datos_pulso))]
+    tiempo = [dt*i for i in range(len(datos_pulso))]
 
     lenD = len(datos_pulso)
     lenT = len(tiempo)
 
-    fs = 1000/40  # Frecuencia de muestreo (Hz)
-    cutoff = 5  # Frecuencia de corte (Hz)
+    fs = 1000/dt  # Frecuencia de muestreo (Hz)
+    cutoff = 3  # Frecuencia de corte (Hz) (entre 2 y 5 va bien)
     b, a = init_filtro_paso_bajo(cutoff, fs)
     zi = None
 
@@ -45,9 +45,17 @@ if __name__ == '__main__':
 
     indices_picos, _ = find_peaks(senal_filtrada, distance=2)
     maximos_tiempo = [[], []]
+    sistolicos_tiempo = [[], []]
+    diastolicos_tiempo = [[], []]
+
     for indice in indices_picos:
         maximos_tiempo[0].append(senal_filtrada[indice])
         maximos_tiempo[1].append(tiempo[indice])
+        # TODO filtrar y añadir picos sistólicos y diastólicos
+        """
+        Posiblemente añadiendo un análisis espectral de la señal usando la Transformada Rápida de Fourier (FFT)
+        + filtrar por rango -> si se cumplen las 2 activar pulso_detectado
+        """
 
     plt.figure(figsize=(13, 6))
     plt.plot(tiempo, datos_pulso, linestyle='-', linewidth=0.5, label="Onda PPG")
