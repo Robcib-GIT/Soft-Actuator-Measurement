8c886ff92f910533b1408aa189896b5a281eee45
AndresSanchezUPM <upmandres@gmail.com>
2024-12-22 18:17:51 +0100

    Nodo de procesamiento cardiaco semiterminado

--- Robot/soft_actuator_measurement/scripts/process_pulse_data.py
+++ Robot/soft_actuator_measurement/scripts/process_pulse_data.py
@@ -30,7 +30,7 @@ MIN_MUESTRAS_ENTRE_PULSOS = (60 / 100) * (1000 / 40)
 MUESTRAS_ENVIO = 5
 SEGMENTOS_ANALISIS = 15
 
-dt = None  # Intervalo de tiempo entre muestras
+dt = 40 # Intervalo de tiempo entre muestras TODO: arreglar actualizacion
 
 
 """
@@ -47,8 +47,8 @@ def process_data():
     while not rospy.is_shutdown():
 
         # 1.1.1: Variables generales
-        processed_fragments = 0
-        finish_processing = False
+        processed_samples = 0
+        finish_processing = False # TODO: Arreglar
 
         # 1.1.2: Variables para el filtro
         fs = 1000 / dt  # Frecuencia de muestreo (Hz)
@@ -59,9 +59,10 @@ def process_data():
         # 1.1.3: Variables para picos
         temp_peaks_time = [np.array([]), np.array([])]
 
-        while not finish_processing:
+        while not rospy.is_shutdown() and not finish_processing:
             filtered_signal = []
 
+            # 1.1.4: Extraer fragmentos de la cola y enviar el fragmento filtrado
             for i in range(SEGMENTOS_ANALISIS):
                 segment = message_queue.get()
 
@@ -74,34 +75,79 @@ def process_data():
                         clipped_segment.append(value)
                     
                     segment = clipped_segment
-                    fin_analisis = True
+                    finish_processing = True
                     break
                 
-                # TODO: enviar -1 al final cuando se acaba y arreglar
                 if len(segment)>0:
                     filtered_segment, zi = apply_low_pass_filter(segment, b, a, zi)
-                    publish_segment(filtered_segment.append(-1))
-                    filtered_signal.append(filtered_segment)
+                    filtered_signal.extend(filtered_segment)
+                
+                if not finish_processing:
+                    publish_segment(filtered_segment)
+                else:
+                    publish_segment(filtered_segment.append(-1))  # Para indicar el fin de la transmision
 
-    segment = []
-    
-    # Por ejemplo, supongamos que realizas algunas operaciones con los elementos del array
-    ppm = sum(segment)  # Operación ficticia
-    ibi = float(segment[0])  # Otro valor ficticio basado en el primer elemento
-    frecuencia = 60.0 / ibi if ibi > 0 else 0.0  # Ejemplo de frecuencia (inversa de IBI)
-    sdnn = 10.0  # Un valor fijo de ejemplo para SDNN
-    rmssd = 5.0  # Un valor fijo de ejemplo para RMSSD
-    
-    # Crear un mensaje CardiacData con los resultados procesados
-    cardiac_data_msg = CardiacData()
-    cardiac_data_msg.ppm = ppm
-    cardiac_data_msg.ibi = ibi
-    cardiac_data_msg.frecuencia = frecuencia
-    cardiac_data_msg.sdnn = sdnn
-    cardiac_data_msg.rmssd = rmssd
-    
-    # Publicar el mensaje CardiacData
-    pub2.publish(cardiac_data_msg)
+            # 1.1.5: Extraer información de la señal
+
+            # Utilizar np.array para mayor velocidad
+            filtered_signal = np.array(filtered_signal)
+            sample_items = len(filtered_signal)
+
+            if sample_items > 0:
+
+                # Añadir tiempo a la señal
+                time = np.arange(processed_samples, processed_samples + sample_items) * dt
+
+                # Unir sobrante anterior para no pasar picos por alto
+                filtered_signal_temp = np.concatenate((temp_peaks_time[0], filtered_signal))
+                time_temp = np.concatenate((temp_peaks_time[1], time))
+
+                # Localizar puntos de interés
+                systolic_indexes, _ = find_peaks(
+                    filtered_signal_temp,
+                    height=(BARRERA_SIS_DIA, MAX_SISTOLICO),
+                    distance=MIN_MUESTRAS_ENTRE_PULSOS)
+                diastolic_indexes, _ = find_peaks(
+                    filtered_signal_temp,
+                    height=(MIN_DIASTOLICO, BARRERA_SIS_DIA),
+                    distance=MIN_MUESTRAS_ENTRE_PULSOS)
+
+                systolics_time = [[], []]
+                diastolics_time = [[], []]
+
+                if len(systolic_indexes) > 0:
+                    for index in systolic_indexes:
+                        systolics_time[0].append(filtered_signal_temp[index])
+                        systolics_time[1].append(time_temp[index])
+
+                if len(diastolic_indexes) > 0:
+                    for index in diastolic_indexes:
+                        diastolics_time[0].append(filtered_signal_temp[index])
+                        diastolics_time[1].append(time_temp[index])
+
+                # Almacenal sobrante por si hay picos
+                if len(filtered_signal) > 2:
+                    clip_index = len(filtered_signal) - 2
+
+                    if len(systolic_indexes) > 0:
+                        if systolic_indexes[-1] > clip_index:
+                            clip_index = None
+                    elif len(diastolic_indexes) > 0:
+                        if diastolic_indexes[-1] > clip_index:
+                            clip_index = None
+
+                    if clip_index is None:
+                        temp_peaks_time = [np.array([]), np.array([])]
+                    else:
+                        temp_peaks_time[0] = filtered_signal[-2:]
+                        temp_peaks_time[1] = time[-2:]
+
+                processed_samples += sample_items     
+
+                # Obtener datos médicos  
+                get_medic_data(systolics_time) 
+
+        rospy.loginfo("Terminado de procesar")
 
 
 # _______________1.X Filtro de paso bajo (Butterworth)_______________
@@ -122,10 +168,39 @@ def apply_low_pass_filter(data, _b, _a, _zi=None):
 
 
 def publish_segment(_segment):
-    msg = Int32MultiArray()
+    msg = Float32MultiArray()
     msg.data = _segment
     msg.layout.data_offset = dt
     pub1.publish(msg)
+
+
+# _______________1.X Obtener y enviar informacion medica_______________
+def get_medic_data(_systolics_time_list):
+
+    # Convertir a numpy para mayor agilidad
+    _systolics_times = np.array(_systolics_time_list[1])
+
+    # Obtener propiedades del pulso
+    if len(_systolics_times) > 1:
+        _ibi = np.diff(_systolics_times)  # Intervalo entre pulsos
+        mean_ibi = np.mean(_ibi)
+        _frequency = 1000 / mean_ibi
+        _pulse = _frequency * 60
+        _sdnn = np.std(_ibi/1000)  # Desviación estándar
+        _rmssd = np.sqrt(np.mean(np.diff(_ibi/1000)**2))  # Raíz cuadrada de la media de las diferencias al cuadrado
+
+        # Enviar informacion
+
+        cardiac_data_msg = CardiacData()
+        cardiac_data_msg.ppm = int(_pulse)
+        cardiac_data_msg.ibi = mean_ibi
+        cardiac_data_msg.frequency = _frequency
+        cardiac_data_msg.sdnn = _sdnn
+        cardiac_data_msg.rmssd = _rmssd
+        
+        # Publicar el mensaje CardiacData
+        pub2.publish(cardiac_data_msg)
+    
 
 
 # _______________1.X Callback para el subscriber_______________
