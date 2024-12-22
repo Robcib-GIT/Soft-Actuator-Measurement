5b0d91f8dfb05f7868589920a45b4f99ef905baf
AndresSanchezUPM <upmandres@gmail.com>
2024-12-22 23:56:13 +0100

    Solucionado error al cerrar programa e inicializaciones

--- Robot/soft_actuator_measurement/scripts/process_pulse_data.py
+++ Robot/soft_actuator_measurement/scripts/process_pulse_data.py
@@ -30,7 +30,8 @@ MIN_MUESTRAS_ENTRE_PULSOS = (60 / 100) * (1000 / 40)
 MUESTRAS_ENVIO = 5
 SEGMENTOS_ANALISIS = 15
 
-dt = 40 # Intervalo de tiempo entre muestras TODO: arreglar actualizacion
+dt = None # Intervalo de tiempo entre muestras TODO: arreglar actualizacion
+finish_processing = True
 
 
 """
@@ -44,110 +45,113 @@ dt = 40 # Intervalo de tiempo entre muestras TODO: arreglar actualizacion
 #   procesados al publicador correspondiente.
 
 def process_data():
+    global finish_processing
+    global dt
     while not rospy.is_shutdown():
 
-        # 1.1.1: Variables generales
-        processed_samples = 0
-        finish_processing = False # TODO: Arreglar
+        if not finish_processing:
+            # 1.1.1: Variables generales
+            processed_samples = 0
 
-        # 1.1.2: Variables para el filtro
-        fs = 1000 / dt  # Frecuencia de muestreo (Hz)
-        cutoff = 3  # Frecuencia de corte (Hz) (entre 2 y 5 va bien)
-        b, a = init_low_pass_filter(cutoff, fs)
-        zi = None
+            # 1.1.2: Variables para el filtro
+            fs = 1000 / dt  # Frecuencia de muestreo (Hz)
+            cutoff = 3  # Frecuencia de corte (Hz) (entre 2 y 5 va bien)
+            b, a = init_low_pass_filter(cutoff, fs)
+            zi = None
 
-        # 1.1.3: Variables para picos
-        temp_peaks_time = [np.array([]), np.array([])]
+            # 1.1.3: Variables para picos
+            temp_peaks_time = [np.array([]), np.array([])]
 
-        while not rospy.is_shutdown() and not finish_processing:
-            filtered_signal = []
+            while not rospy.is_shutdown() and not finish_processing:
+                filtered_signal = []
 
-            # 1.1.4: Extraer fragmentos de la cola y enviar el fragmento filtrado
-            for i in range(SEGMENTOS_ANALISIS):
-                segment = message_queue.get()
+                # 1.1.4: Extraer fragmentos de la cola y enviar el fragmento filtrado
+                for i in range(SEGMENTOS_ANALISIS):
+                    segment = message_queue.get()
 
-                if -1 in segment:
-                    clipped_segment = []
+                    if -1 in segment:
+                        clipped_segment = []
 
-                    for value in segment:
-                        if value == -1:
-                            break
-                        clipped_segment.append(value)
+                        for value in segment:
+                            if value == -1:
+                                break
+                            clipped_segment.append(value)
+                        
+                        segment = clipped_segment
+                        finish_processing = True
+                        break
                     
-                    segment = clipped_segment
-                    finish_processing = True
-                    break
-                
-                if len(segment)>0:
-                    filtered_segment, zi = apply_low_pass_filter(segment, b, a, zi)
-                    filtered_signal.extend(filtered_segment)
-                
-                if not finish_processing:
-                    publish_segment(filtered_segment)
-                else:
-                    publish_segment(filtered_segment.append(-1))  # Para indicar el fin de la transmision
+                    if len(segment)>0:
+                        filtered_segment, zi = apply_low_pass_filter(segment, b, a, zi)
+                        filtered_signal.extend(filtered_segment)
+                    
+                    if not finish_processing:
+                        publish_segment(filtered_segment)
+                    else:
+                        publish_segment(filtered_segment.append(-1))  # Para indicar el fin de la transmision
 
-            # 1.1.5: Extraer información de la señal
+                # 1.1.5: Extraer información de la señal
 
-            # Utilizar np.array para mayor velocidad
-            filtered_signal = np.array(filtered_signal)
-            sample_items = len(filtered_signal)
+                # Utilizar np.array para mayor velocidad
+                filtered_signal = np.array(filtered_signal)
+                sample_items = len(filtered_signal)
 
-            if sample_items > 0:
+                if sample_items > 0:
 
-                # Añadir tiempo a la señal
-                time = np.arange(processed_samples, processed_samples + sample_items) * dt
+                    # Añadir tiempo a la señal
+                    time = np.arange(processed_samples, processed_samples + sample_items) * dt
 
-                # Unir sobrante anterior para no pasar picos por alto
-                filtered_signal_temp = np.concatenate((temp_peaks_time[0], filtered_signal))
-                time_temp = np.concatenate((temp_peaks_time[1], time))
+                    # Unir sobrante anterior para no pasar picos por alto
+                    filtered_signal_temp = np.concatenate((temp_peaks_time[0], filtered_signal))
+                    time_temp = np.concatenate((temp_peaks_time[1], time))
 
-                # Localizar puntos de interés
-                systolic_indexes, _ = find_peaks(
-                    filtered_signal_temp,
-                    height=(BARRERA_SIS_DIA, MAX_SISTOLICO),
-                    distance=MIN_MUESTRAS_ENTRE_PULSOS)
-                diastolic_indexes, _ = find_peaks(
-                    filtered_signal_temp,
-                    height=(MIN_DIASTOLICO, BARRERA_SIS_DIA),
-                    distance=MIN_MUESTRAS_ENTRE_PULSOS)
+                    # Localizar puntos de interés
+                    systolic_indexes, _ = find_peaks(
+                        filtered_signal_temp,
+                        height=(BARRERA_SIS_DIA, MAX_SISTOLICO),
+                        distance=MIN_MUESTRAS_ENTRE_PULSOS)
+                    diastolic_indexes, _ = find_peaks(
+                        filtered_signal_temp,
+                        height=(MIN_DIASTOLICO, BARRERA_SIS_DIA),
+                        distance=MIN_MUESTRAS_ENTRE_PULSOS)
 
-                systolics_time = [[], []]
-                diastolics_time = [[], []]
-
-                if len(systolic_indexes) > 0:
-                    for index in systolic_indexes:
-                        systolics_time[0].append(filtered_signal_temp[index])
-                        systolics_time[1].append(time_temp[index])
-
-                if len(diastolic_indexes) > 0:
-                    for index in diastolic_indexes:
-                        diastolics_time[0].append(filtered_signal_temp[index])
-                        diastolics_time[1].append(time_temp[index])
-
-                # Almacenal sobrante por si hay picos
-                if len(filtered_signal) > 2:
-                    clip_index = len(filtered_signal) - 2
+                    systolics_time = [[], []]
+                    diastolics_time = [[], []]
 
                     if len(systolic_indexes) > 0:
-                        if systolic_indexes[-1] > clip_index:
-                            clip_index = None
-                    elif len(diastolic_indexes) > 0:
-                        if diastolic_indexes[-1] > clip_index:
-                            clip_index = None
+                        for index in systolic_indexes:
+                            systolics_time[0].append(filtered_signal_temp[index])
+                            systolics_time[1].append(time_temp[index])
 
-                    if clip_index is None:
-                        temp_peaks_time = [np.array([]), np.array([])]
-                    else:
-                        temp_peaks_time[0] = filtered_signal[-2:]
-                        temp_peaks_time[1] = time[-2:]
+                    if len(diastolic_indexes) > 0:
+                        for index in diastolic_indexes:
+                            diastolics_time[0].append(filtered_signal_temp[index])
+                            diastolics_time[1].append(time_temp[index])
 
-                processed_samples += sample_items     
+                    # Almacenal sobrante por si hay picos
+                    if len(filtered_signal) > 2:
+                        clip_index = len(filtered_signal) - 2
 
-                # Obtener datos médicos  
-                get_medic_data(systolics_time) 
+                        if len(systolic_indexes) > 0:
+                            if systolic_indexes[-1] > clip_index:
+                                clip_index = None
+                        elif len(diastolic_indexes) > 0:
+                            if diastolic_indexes[-1] > clip_index:
+                                clip_index = None
 
-        rospy.loginfo("Terminado de procesar")
+                        if clip_index is None:
+                            temp_peaks_time = [np.array([]), np.array([])]
+                        else:
+                            temp_peaks_time[0] = filtered_signal[-2:]
+                            temp_peaks_time[1] = time[-2:]
+
+                    processed_samples += sample_items     
+
+                    # Obtener datos médicos  
+                    get_medic_data(systolics_time) 
+
+            rospy.loginfo("Terminado de procesar")
+            dt = None
 
 
 # _______________1.X Filtro de paso bajo (Butterworth)_______________
@@ -205,10 +209,16 @@ def get_medic_data(_systolics_time_list):
 
 # _______________1.X Callback para el subscriber_______________
 def callback(msg):
-    # Establecer sample time
     global dt
+    global finish_processing
+
+    # Establecer sample time
     if dt is None:
         dt = msg.layout.data_offset
+
+    # Iniciar procesamiento
+    if finish_processing == True:
+        finish_processing = False
     
     # Añadir fragmento a la colaa
     message_queue.put(msg.data)
