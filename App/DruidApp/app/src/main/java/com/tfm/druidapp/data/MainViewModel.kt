e77041c2ba4d958c735dcca2ea75a7e13d05807c
AndresSanchezUPM <upmandres@gmail.com>
2025-01-18 17:07:05 +0100

    Añadir rangos de valores normales

--- App/DruidApp/app/src/main/java/com/tfm/druidapp/data/MainViewModel.kt
+++ App/DruidApp/app/src/main/java/com/tfm/druidapp/data/MainViewModel.kt
@@ -135,8 +135,9 @@ class MainViewModel(private val dataStoreManager: DataStoreManager) : ViewModel(
 
     private val _normalMedicRanges = mutableStateOf(NormalMedicRanges())
     val normalMedicRanges: State<NormalMedicRanges> get() = _normalMedicRanges
-    fun updateNormalMedicRanges(){
+    private fun updateNormalMedicRanges(){
         //TODO: hacer
+        _normalMedicRanges.value = calculateNormalRanges(age = settingsData.value.age, gender = settingsData.value.gender)
     }
 
     //Glasgow
--- App/DruidApp/app/src/main/java/com/tfm/druidapp/data/MedicData.kt
+++ App/DruidApp/app/src/main/java/com/tfm/druidapp/data/MedicData.kt
@@ -1,79 +1,169 @@
 package com.tfm.druidapp.data
 
-import android.content.res.Configuration
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.tfm.druidapp.ui.theme.DruidAppTheme
-import com.tfm.druidapp.views.PPGView
-
 ////////////////////// MEDIC INFO //////////////////////
 data class NormalRange<T : Number>(
     val min: T,
     val max: T
 )
+
 data class NormalMedicRanges(
     val ppm: NormalRange<Int>? = null,
-    val ibi: NormalRange<Float>? = null,
-    val frequency: NormalRange<Float>? = null,
-    val sdnn: NormalRange<Float>? = null,
-    val rmsdd: NormalRange<Float>? = null,
-    val amplitude: NormalRange<Float>? = null,
-    val riseTime: NormalRange<Float>? = null,
+    //val ibi: NormalRange<Float>? = null,
+    //val frequency: NormalRange<Float>? = null,
+    val sdnn: NormalRange<Float> = NormalRange(50f,150f),
+    val rmsdd: NormalRange<Float> = NormalRange(20f,50f),
+    //val amplitude: NormalRange<Float>? = null,
+    val riseTime: NormalRange<Float> = NormalRange(100f,200f),
     val sys: NormalRange<Int>? = null,
     val dia: NormalRange<Int>? = null,
-    val temperature: NormalRange<Float>? = null
+    val temperature: NormalRange<Float> = NormalRange(35f,37.7f)
 )
+
+fun calculateNormalRanges(age: Int, gender: String): NormalMedicRanges {
+    var ranges = NormalMedicRanges()
+
+    //Ritmo cardíaco
+    ranges = when{
+        age <= 1->{ranges.copy(ppm = NormalRange(70,190))}
+        age <= 2->{ranges.copy(ppm = NormalRange(80,130))}
+        age <= 4->{ranges.copy(ppm = NormalRange(80,120))}
+        age <= 6->{ranges.copy(ppm = NormalRange(75,115))}
+        age <= 9->{ranges.copy(ppm = NormalRange(70,110))}
+        else->{ranges.copy(ppm = NormalRange(60,100))}
+    }
+
+    //Presión arterial
+    ranges = when {
+        age <= 18 -> {
+            if (gender == "Hombre") {
+                ranges.copy(
+                    sys = NormalRange(105, 135),
+                    dia = NormalRange(60, 86)
+                )
+            } else {
+                ranges.copy(
+                    sys = NormalRange(100, 130),
+                    dia = NormalRange(60, 85)
+                )
+            }
+        }
+        age <= 24 -> {
+            if (gender == "Hombre") {
+                ranges.copy(
+                    sys = NormalRange(105, 139),
+                    dia = NormalRange(62, 88)
+                )
+            } else {
+                ranges.copy(
+                    sys = NormalRange(100, 130),
+                    dia = NormalRange(60, 85)
+                )
+            }
+        }
+        age <= 29 -> {
+            if (gender == "Hombre") {
+                ranges.copy(
+                    sys = NormalRange(108, 139),
+                    dia = NormalRange(65, 89)
+                )
+            } else {
+                ranges.copy(
+                    sys = NormalRange(102, 135),
+                    dia = NormalRange(60, 86)
+                )
+            }
+        }
+        age <= 39 -> {
+            if (gender == "Hombre") {
+                ranges.copy(
+                    sys = NormalRange(110, 145),
+                    dia = NormalRange(68, 92)
+                )
+            } else {
+                ranges.copy(
+                    sys = NormalRange(105, 139),
+                    dia = NormalRange(65, 89)
+                )
+            }
+        }
+        age <= 49 -> {
+            if (gender == "Hombre") {
+                ranges.copy(
+                    sys = NormalRange(110, 150),
+                    dia = NormalRange(70, 96)
+                )
+            } else {
+                ranges.copy(
+                    sys = NormalRange(105, 150),
+                    dia = NormalRange(65, 96)
+                )
+            }
+        }
+        age <= 59 -> {
+            if (gender == "Hombre") {
+                ranges.copy(
+                    sys = NormalRange(115, 155),
+                    dia = NormalRange(70, 98)
+                )
+            } else {
+                ranges.copy(
+                    sys = NormalRange(110, 155),
+                    dia = NormalRange(70, 98)
+                )
+            }
+        }
+        else -> {
+            if (gender == "Hombre") {
+                ranges.copy(
+                    sys = NormalRange(115, 160),
+                    dia = NormalRange(70, 100)
+                )
+            } else {
+                ranges.copy(
+                    sys = NormalRange(115, 160),
+                    dia = NormalRange(70, 100)
+                )
+            }
+        }
+    }
+
+    return ranges
+}
+
 ////////////////////// GLASGOW //////////////////////
 data class GlasgowTestItem(
     val text: String,
     val score: Int
 )
 
-sealed class GlasgowData(val options: List<GlasgowTestItem>){
-    data object EyeOpening:GlasgowData(
+sealed class GlasgowData(val options: List<GlasgowTestItem>) {
+    data object EyeOpening : GlasgowData(
         listOf(
-            GlasgowTestItem("Espontánea",4),
-            GlasgowTestItem("A la orden",3),
-            GlasgowTestItem("Ante estímulo doloroso",2),
-            GlasgowTestItem("Ausencia de apertura ocular",1)
+            GlasgowTestItem("Espontánea", 4),
+            GlasgowTestItem("A la orden", 3),
+            GlasgowTestItem("Ante estímulo doloroso", 2),
+            GlasgowTestItem("Ausencia de apertura ocular", 1)
         )
     )
-    data object VerbalResponse:GlasgowData(
+
+    data object VerbalResponse : GlasgowData(
         listOf(
-            GlasgowTestItem("Orientado correctamente",5),
-            GlasgowTestItem("Paciente confuso",4),
-            GlasgowTestItem("Lenguaje inapropiado",3),
-            GlasgowTestItem("Lenguaje incomprensible",2),
-            GlasgowTestItem("Carencia de actividad verbal",1)
+            GlasgowTestItem("Orientado correctamente", 5),
+            GlasgowTestItem("Paciente confuso", 4),
+            GlasgowTestItem("Lenguaje inapropiado", 3),
+            GlasgowTestItem("Lenguaje incomprensible", 2),
+            GlasgowTestItem("Carencia de actividad verbal", 1)
         )
     )
-    data object MotorResponse:GlasgowData(
+
+    data object MotorResponse : GlasgowData(
         listOf(
-            GlasgowTestItem("Obedece órdenes correctamente",6),
-            GlasgowTestItem("Localiza estímulos dolorosos",5),
-            GlasgowTestItem("Responde al estímulo doloroso pero no localiza",4),
-            GlasgowTestItem("Respuesta con flexión anormal de los miembros",3),
-            GlasgowTestItem("Respuesta con extensión anormal de los miembros",2),
-            GlasgowTestItem("Ausencia de respuesta motora",1)
+            GlasgowTestItem("Obedece órdenes correctamente", 6),
+            GlasgowTestItem("Localiza estímulos dolorosos", 5),
+            GlasgowTestItem("Responde al estímulo doloroso pero no localiza", 4),
+            GlasgowTestItem("Respuesta con flexión anormal de los miembros", 3),
+            GlasgowTestItem("Respuesta con extensión anormal de los miembros", 2),
+            GlasgowTestItem("Ausencia de respuesta motora", 1)
         )
     )
 }
