1b234f19e1bfe119e02b85ad8ffb2a7a1b58e331
MyJetson <upmandres@gmail.com>
2025-01-29 19:58:16 +0100

    ns

--- Otros/Pruebas/Python/PressureSensor/hx710B/prueba_ready.py
+++ Otros/Pruebas/Python/PressureSensor/hx710B/prueba_ready.py
@@ -8,7 +8,6 @@ Para device 0:
 MOSI: 19 | MISO: 21 | SCK: 23
 """
 
-
 spi = spidev.SpiDev()
 spi.open(0, 0)
 spi.max_speed_hz = 1000000     # 2 * 1/(2e-6)
@@ -17,7 +16,7 @@ spi.bits_per_word = 8
 
 try:
     while True:
-        respuesta = spi.xfer2([0x0])
+        respuesta = spi.readbytes(1)
         print(f"Respuesta: {bin(respuesta[0])} len: {len(respuesta)}")
         time.sleep(2)
 
