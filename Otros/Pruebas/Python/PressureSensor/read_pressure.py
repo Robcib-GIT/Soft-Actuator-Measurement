1883aa5506cd1bdcb7d30bdb1bd7ccef10c25a58
AndresSanchezUPM <upmandres@gmail.com>
2025-01-31 19:44:33 +0100

    pruebas spi

--- Otros/Pruebas/Python/PressureSensor/read_pressure.py
+++ Otros/Pruebas/Python/PressureSensor/read_pressure.py
@@ -3,28 +3,26 @@ import time
 
 # Configurar SPI
 spi = spidev.SpiDev()
-spi.open(0, 0)  # Open SPI bus 0, device (CS) 0
+spi.open(0, 1)  # Open SPI bus 0, device (CS) 0
 
 
 # Configurar velocidad de reloj (ajustable)
-spi.max_speed_hz = 500000  # 500 kHz
-spi.mode = 0b00  # CPOL = 0, CPHA = 0
-spi.bits_per_word = 8  # SPI bits per word
+spi.max_speed_hz = 10000  # 500 kHz
+spi.mode = 0b10  # CPOL = 0, CPHA = 0
 
 
 
 def read_pressure():
-    # Send 3 bytes of zeros to read 24 bits of data
-    raw_data = spi.xfer2([0, 0, 0])  # Enviar 3 bytes vacíos para generar los pulsos
-
-    # Combine the 3 bytes into a single 24-bit value
-    resultado = (raw_data[0] << 16) | (raw_data[1] << 8) | raw_data[2]
+    
+    # Send 24 empty bits (3 bytes of zeros)
+    raw_data = spi.xfer2([0x00, 0x00, 0x00])  # Enviar 3 bytes vacíos para generar los pulsos
+    #resultado = (raw_data[0] << 16) | (raw_data[1] << 8) | raw_data[2]  # Combinar los 3 bytes
 
     # Convertir a valor entero (24 bits, complemento a dos)
-    if resultado & 0x800000:  # Si el bit más alto está en 1 (negativo en complemento a dos)
-        resultado -= 0x1000000
+    #if resultado & 0x800000:  # Si el bit más alto está en 1 (negativo en complemento a dos)
+    #    resultado -= 0x1000000
 
-    return resultado
+    return raw_data
 
     
 
