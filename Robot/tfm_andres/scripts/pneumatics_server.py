5ae1722d144b565e2be2688d835a639f966603db
AndresSanchezUPM <upmandres@gmail.com>
2025-02-17 21:29:51 +0100

    Avance con action neumatica

--- Robot/tfm_andres/scripts/pneumatics_server.py
+++ Robot/tfm_andres/scripts/pneumatics_server.py
@@ -11,20 +11,28 @@ actuator_pressure_goal = 200.0 #TODO cambiar
 cuff_pressure_goal = 300.0  #TODO cambiar
 
 @dataclass
+class PressureGoal:
+    valve: int
+    pressure: float
+
+@dataclass
 class PneumaticState:
     name: str
     pump_on: bool
     valve1_angle: Optional[int]
     valve2_angle: Optional[int]
+    goal: Optional[PressureGoal]
+
+
 
 class PneumaticsServer:
     STATES = [ #TODO igual añadir algun tipo de funcion ns con callable
-        PneumaticState(name="Home", pump_on=False, valve1_angle=90, valve2_angle=90),
-        PneumaticState(name="Close actuator", pump_on=True, valve1_angle=90, valve2_angle=None),
-        PneumaticState(name="Inflate cuff", pump_on=True, valve1_angle=0, valve2_angle=180),
-        PneumaticState(name="Deflate cuff", pump_on=False, valve1_angle=90, valve2_angle=None),
-        PneumaticState(name="Open actuator", pump_on=False, valve1_angle=180, valve2_angle=180),
-        PneumaticState(name="IDLE", pump_on=False, valve1_angle=None, valve2_angle=None)
+        PneumaticState(name="Home", pump_on=False, valve1_angle=90, valve2_angle=90, goal=None),
+        PneumaticState(name="Close actuator", pump_on=True, valve1_angle=90, valve2_angle=None, goal=PressureGoal(valve=2, pressure=actuator_pressure_goal)),
+        PneumaticState(name="Inflate cuff", pump_on=True, valve1_angle=0, valve2_angle=180, goal=PressureGoal(valve=1, pressure=cuff_pressure_goal)),
+        PneumaticState(name="Deflate cuff", pump_on=False, valve1_angle=90, valve2_angle=None, goal=None),
+        PneumaticState(name="Open actuator", pump_on=False, valve1_angle=180, valve2_angle=180, goal=None),
+        PneumaticState(name="IDLE", pump_on=False, valve1_angle=None, valve2_angle=None, goal=None)
     ]
 
     def __init__(self):
@@ -56,15 +64,16 @@ class PneumaticsServer:
         rate = rospy.Rate(2)  # 2 Hz TODO cambiar
 
         # Ejecutar la secuencia de válvulas
-        for task in goal.tasks:
+        for state in range(goal.first_state, goal.last_state + 1):
             if self.server.is_preempt_requested():
                 rospy.logwarn("Acción pneumática interrumpida.")
                 self.server.set_preempted()
                 return
             
             progress = 0
-            self.setValves(task)
-            rospy.sleep(2)
+            self.setValves(state)
+
+            #TODO añadir algo para start pressure
 
             while progress<1:
                 progress = self.calculateProgress(task)
@@ -81,31 +90,27 @@ class PneumaticsServer:
         self.server.set_succeeded(result)
         rospy.loginfo("Pneumatics sequence completed successfully.")
 
-    def setValves(task: String): #TODO completar
-        if task == "home":
-            print("Going to home position")
-            #TODO
-        elif task == "close actuator":
-            print("Closing actuator")
-            #TODO
-        elif task == "Pump cuff":
-            print("Pumping Up cuff")
-            #TODO
-        else: #IDLE
-            pass
+    def setValves(self, state: int): #TODO completar
+        self.turnValve(valve=1, angle=PneumaticsServer.STATES[state].valve1_angle)
+        self.turnValve(valve=2, angle=PneumaticsServer.STATES[state].valve2_angle)
     
-    def calculateProgress(task: String): #TODO completar + returns
-        if task == "home":
-            print("Going to home position")
-            #TODO
-        elif task == "close actuator":
-            print("Closing actuator")
-            #TODO
-        elif task == "Pump cuff":
-            print("Pumping Up cuff")
-            #TODO
-        else: #IDLE
-            pass
+    def calculateProgress(self, state_id: int, start_pressure: float): #TODO completar + returns
+        progress = 0
+        try:
+            state = PneumaticsServer.STATES[state_id]
+            if state.goal is None:
+                rospy.sleep(3) #TODO configurar
+                progress = 1
+            else:
+                if state.goal.valve == 1: 
+                    progress = abs(self.cuff_pressure - start_pressure)/abs(state.goal.pressure - start_pressure)
+                else:
+                    progress = abs(self.actuator_pressure - start_pressure)/abs(state.goal.pressure - start_pressure)
+            
+            return progress #TODO completar esto porque naroa no me deja acabar
+            
+        except IndexError:
+            print("Índice fuera de rango")
 
     def switchOnPump(): #TODO añadir variable global y en set valves meter esta funcion cuando toque solo para cambiar
         print("Swintching on pump")
@@ -123,7 +128,7 @@ class PneumaticsServer:
 
 if __name__ == '__main__':
     try:
-        ValveSequenceServer()
+        PneumaticsServer()
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
