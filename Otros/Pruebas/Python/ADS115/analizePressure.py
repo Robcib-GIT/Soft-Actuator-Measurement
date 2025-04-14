326396c2bc51ae9e3d68c246d9ed7cf0f0df521b
AndresSanchezUPM <upmandres@gmail.com>
2025-04-14 15:00:26 +0200

    creo que presion bien

--- Otros/Pruebas/Python/ADS115/analizePressure.py
+++ Otros/Pruebas/Python/ADS115/analizePressure.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from scipy.signal import butter, filtfilt
+from scipy.signal import butter, filtfilt, find_peaks, peak_prominences
 from tkinter import Tk, filedialog
 
 
@@ -52,13 +52,58 @@ derivada_presion = calcular_derivada(tiempo, presion)
 fs = 1 / np.mean(np.diff(tiempo))  # Hz
 
 # Paso 5: Filtrar derivada
-derivada_filtrada = filtro_paso_bajo(derivada_presion, fs, fc=2.5)
+fc = 2.5
+derivada_filtrada = filtro_paso_bajo(derivada_presion, fs, fc=fc)
+
+# Encontrar maximo presion
+max_presion = np.max(presion)
+indice_max_presion = np.argmax(presion) + 10 #offset para evitar cosas raras
+
+# Encontrar picos vPresion
+indices_picos_vPresion, _ = find_peaks(
+    derivada_filtrada,
+    height=(-25, -4),  # TODO: ajustar
+    prominence=1.5,  # Primero filtrar por el maximo de las bases
+    # width=(fs*60/180.0, fs*60/40.0) TODO:
+)
+width=(fs*60/200.0, fs*60/20.0)
+picos_vPresion = [derivada_filtrada[i] for i in indices_picos_vPresion]
+tiempo_picos_vPresion = [tiempo[i] for i in indices_picos_vPresion]
+
+prominencias, izq_bases, der_bases = peak_prominences(derivada_filtrada, indices_picos_vPresion)
+
+# Calcula "prominencia alternativa" usando el mínimo de las dos bases
+bases_min = np.minimum(derivada_filtrada[izq_bases], derivada_filtrada[der_bases])
+prominencia_min = derivada_filtrada[indices_picos_vPresion] - bases_min
+
+# Filtrar los picos cuya prominencia mínima sea mayor que cierto valor, por ejemplo 1.5
+umbral = 5
+#indices_picos_filtrados = [indices_picos_vPresion[i] for i, prominencia in enumerate(prominencia_min) if prominencia > umbral]
+indices_picos_filtrados = []
+
+# Recorremos cada elemento de la lista prominencia_min con su índice
+for i in range(len(prominencia_min)):
+    # Si la prominencia es mayor que el umbral
+    if prominencia_min[i] > umbral and indices_picos_vPresion[i]>indice_max_presion:
+        # Añadimos el índice correspondiente de indices_picos_vPresion a la nueva lista
+        indices_picos_filtrados.append(indices_picos_vPresion[i])
+new_picos_vPresion = [derivada_filtrada[i] for i in indices_picos_filtrados]
+new_tiempo_picos_vPresion = [tiempo[i] for i in indices_picos_filtrados]
+
+# Calcular presiones sys y dia
+indice_sys = indices_picos_filtrados[0]
+sys = int(presion[indice_sys])
+indice_dia = indices_picos_filtrados[-1]
+dia = int(presion[indice_dia])
+print(f"Sys: {sys}mmHg  |  Dia: {dia}mmHg")
 
 # Paso 6: Graficar
 plt.figure(figsize=(12, 6))
 
 plt.subplot(2, 1, 1)
 plt.plot(tiempo, presion, label='Presión original')
+plt.scatter(tiempo[indice_sys], presion[indice_sys], color='blue', marker='o', label=f"SYS: {sys}mmHg")
+plt.scatter(tiempo[indice_dia], presion[indice_dia], color='green', marker='o', label=f"DIA: {dia}mmHg")
 plt.xlabel('Tiempo (s)')
 plt.ylabel('Presión (mmHg)')
 plt.title('Presión vs Tiempo')
@@ -67,7 +112,8 @@ plt.legend()
 
 plt.subplot(2, 1, 2)
 plt.plot(tiempo, derivada_presion, label='Derivada sin filtrar', alpha=0.6)
-plt.plot(tiempo, derivada_filtrada, label='Derivada filtrada (20 Hz)', linewidth=2)
+plt.plot(tiempo, derivada_filtrada, label=f'Derivada filtrada ({fc} Hz)', linewidth=2)
+plt.scatter(new_tiempo_picos_vPresion, new_picos_vPresion, color='red', marker='o', label='Picos detectados')
 plt.xlabel('Tiempo (s)')
 plt.ylabel('dPresión/dt (mmHg/s)')
 plt.title('Primera derivada de la presión')
