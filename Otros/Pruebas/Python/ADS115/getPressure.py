d0240a85d2af423b29d7aae88de93204aa4231d9
AndresSanchezUPM <upmandres@gmail.com>
2025-04-26 15:47:24 +0200

    ajustes sensores presion

--- Otros/Pruebas/Python/ADS115/getPressure.py
+++ Otros/Pruebas/Python/ADS115/getPressure.py
@@ -1,11 +1,15 @@
 import ADS1x15
 import time
 
-ADS = ADS1x15.ADS1115(0, 0x48)   
+ADS = ADS1x15.ADS1115(1, 0x48)   
 ADS.setGain(ADS.PGA_0_512V)
 
-def getPressure(offset=27.0, pressureRef = 200.0, valueRef=2960.0):
-    pressure_raw = ADS.readADC_Differential_0_1()
+def getPressure(sensor: int = 1, offset=27.0, pressureRef = 200.0, valueRef=2960.0):
+    if sensor == 1:
+        pressure_raw = ADS.readADC_Differential_0_1()
+    else:
+        pressure_raw = ADS.readADC_Differential_2_3()
+
     pressure = (pressure_raw-offset)*pressureRef/valueRef
     return float(pressure)
 
@@ -14,8 +18,9 @@ interval = 0.1  # Cambia este valor por el que necesites
 
 try:
     while True:
-        pressure = getPressure()
-        print(f"\rPresión: {pressure:.2f} mmHg     ", end="")
+        pressure1 = getPressure(sensor=1)
+        pressure2 = getPressure(sensor=2)
+        print(f"\rPresión manguito: {pressure1:.2f} mmHg    |   Presión actuador: {pressure2:.2f} mmHg     ", end="")
         time.sleep(interval)
 except KeyboardInterrupt:
     print("\nLectura interrumpida por el usuario.")
