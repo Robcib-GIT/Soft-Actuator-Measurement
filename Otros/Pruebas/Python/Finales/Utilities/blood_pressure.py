5a050f32441418ae45786be618e250ef4ab32c16
AndresSanchezUPM <upmandres@gmail.com>
2025-05-08 11:34:36 +0200

    encontrado fallo de prominencia

--- Otros/Pruebas/Python/Finales/Utilities/blood_pressure.py
+++ Otros/Pruebas/Python/Finales/Utilities/blood_pressure.py
@@ -7,16 +7,16 @@ from scipy.signal import butter, filtfilt, find_peaks, peak_prominences
 
 
 class BloodPressure:
-    __PROMINENCE_MIN = 1.5    # Prominencia mínima para considerar un pico
-    __PROMINENCE_MAX = 5      # Prominencia máxima para considerar un pico
-    __HEIGHT_MAX = 2          # Altura máxima permitida para los picos
-    __HEIGHT_MIN = -40        # Altura mínima permitida para los picos
-    __FC = 2                  # Frecuencia de corte del filtro paso bajo para la derivada de la señal
+    __PROMINENCE_MIN = 4  # Prominencia mínima para considerar un pico
+    __PROMINENCE_MAX = 1000  # Prominencia máxima para considerar un pico  TODO: ver a ver por que con 1000 si si es biende
+    __HEIGHT_MAX = 5  # Altura máxima permitida para los picos
+    __HEIGHT_MIN = -20  # Altura mínima permitida para los picos
+    __FC = 4  # Frecuencia de corte del filtro paso bajo para la derivada de la señal
 
     def __init__(self, fs: float):
         self.fs = fs
         self.sample_interval = 1 / fs
-        self.pressures: np.ndarray | None = None    # TODO: comprobar que en ros no me de problema
+        self.pressures: np.ndarray | None = None  # TODO: comprobar que en ros no me de problema
         self.time: np.ndarray | None = None
         self.d_pressures: np.ndarray | None = None
         self.d_pressures_filtered: np.ndarray | None = None
@@ -39,19 +39,20 @@ class BloodPressure:
 
     # Filtrado morfológico de picos: solo se aceptan picos entre ciertos valores de altura y prominencia situados en la zona de desinflado
     # Devuelve los índices en los que se encuentran los picos
-    def __morphological_filter(self, signal: np.ndarray, idx_start: int) -> List[int]:
-        idx_peaks, _ = find_peaks(signal, height=(self.__HEIGHT_MIN, self.__HEIGHT_MAX), prominence=self.__PROMINENCE_MIN)
+    def __morphological_filter(self, signal: np.ndarray, idx_start: int) -> List[int]:  # TODO: no sirve la prominencia cambiarlo
+        idx_peaks, _ = find_peaks(signal, height=(self.__HEIGHT_MIN, self.__HEIGHT_MAX),
+                                  prominence=self.__PROMINENCE_MIN)
         prominences, left_bases, right_bases = peak_prominences(signal, idx_peaks)
-        min_prominences = signal[idx_peaks] - np.minimum(signal[left_bases], signal[right_bases])
-        return [n for i, n in enumerate(idx_peaks) if min_prominences[i] > self.__PROMINENCE_MAX and n > idx_start]
+        max_prominences = signal[idx_peaks] - np.minimum(signal[left_bases], signal[right_bases])
+        return [n for i, n in enumerate(idx_peaks) if max_prominences[i] <= self.__PROMINENCE_MAX and n > idx_start]   # TODO: mirar si < o >
 
     # Filtrado basado en distanciamiento entre picos: se buscan secuencias de picos regulares
     # y compatibles con la frecuencia cardíaca
-    def __distance_based_filter(self, peaks: List[int]) -> List[int]:
+    def __distance_based_filter(self, peaks: List[int]) -> List[int]:  # TODO: mirar si pillar ambos lados en vez de uno
         distances = np.diff(peaks)
         min_d = math.floor(60 / 230 * self.fs)  # Muestras correspondientes a 230ppm
         max_d = math.ceil(60 / 40 * self.fs)  # Muestras correspondientes a 40ppm
-        var = math.ceil(60E-3 * self.fs)  # Maxima diferencia entre pulsos de 60ms=60E-3*fs muestras
+        var = math.ceil(80E-3 * self.fs)  # Maxima diferencia entre pulsos de 60ms=60E-3*fs muestras  # FIXME: he cambiado a 80 mirar histograma
 
         # Obtener histograma y obtener la distancia objetivo como la más frecuente
         bins = np.arange(min_d, max_d + 1, var)
@@ -114,8 +115,46 @@ class BloodPressure:
         plt.tight_layout()
         plt.show()
 
+    def plot_pruebas(self, idx_peaks):  # TODO: borrar!!!!
+        peaks_time = [self.time[i] for i in idx_peaks]
+        peaks_d_pressure = [self.d_pressures_filtered[i] for i in idx_peaks]
+        idx_sys = idx_peaks[0]
+        idx_dia = idx_peaks[-1]
+
+        sys, dia = int(self.pressures[idx_sys]), int(self.pressures[idx_dia])
+
+        plt.figure(figsize=(12, 6))
+
+        # Gráfico de presión original con anotaciones de presión sistólica y diastólica
+        plt.subplot(2, 1, 1)
+        plt.plot(self.time, self.pressures, label='Original pressure')
+        plt.scatter(self.time[idx_sys], self.pressures[idx_sys], color='blue', label=f"SYS: {sys} mmHg")
+        plt.axvline(x=self.time[idx_sys], color='r', linestyle='--')
+        plt.scatter(self.time[idx_dia], self.pressures[idx_dia], color='green', label=f"DIA: {dia} mmHg")
+        plt.axvline(x=self.time[idx_dia], color='r', linestyle='--')
+        plt.xlabel('Time (s)')
+        plt.ylabel('Pressure (mmHg)')
+        plt.title('Pressure vs Time')
+        plt.grid()
+        plt.legend()
+
+        plt.subplot(2, 1, 2)
+
+        plt.plot(self.time, self.d_pressures, label='Raw derivative', alpha=0.6)
+        plt.plot(self.time, self.d_pressures_filtered, label=f'Filtered derivative ({self.__FC} Hz)', linewidth=2)
+        plt.scatter(peaks_time, peaks_d_pressure, color='red', label='Detected morph peaks')
+        plt.xlabel('Time (s)')
+        plt.ylabel('d_Pressure (mmHg/s)')
+        plt.title('d_Pressure vs Time')
+        plt.grid()
+        plt.legend()
+
+        print(f"Resultados de filtro morfológico: SYS: {sys}  |  DIA: {dia}")
+
+        plt.show()
+
     # Calcular velocidad media
-    def calculate_velocity(self, pressures: List[float], sample_time=0.5):   #TODO: hacer que sea la media de todas
+    def calculate_velocity(self, pressures: List[float], sample_time=0.5):  # TODO: hacer que sea la media de todas
         samples = max(2, math.ceil(sample_time * self.fs))  # al menos 2 muestras
         if len(pressures) < samples:
             return 0.0
@@ -134,11 +173,14 @@ class BloodPressure:
             self.d_pressures_filtered = self.__lowpass_filter()
 
             idx_deflating_point = int(np.argmax(pressures))  # Índice del punto de desinflado
-            idx_morph_peaks = self.__morphological_filter(signal=self.d_pressures_filtered, idx_start=idx_deflating_point)
+            idx_morph_peaks = self.__morphological_filter(signal=self.d_pressures_filtered,
+                                                          idx_start=idx_deflating_point)
             if len(idx_morph_peaks) < 2:
                 raise ValueError("No se han encontrado picos en el análisis morfológico.")
 
-            idx_distance_peaks = self.__distance_based_filter(idx_morph_peaks)
+            self.plot_pruebas(idx_morph_peaks)  # TODO: quitar despues de las pruebas
+
+            idx_distance_peaks = self.__distance_based_filter(idx_morph_peaks)  # Descomentar para ver lo que hace
             if len(idx_distance_peaks) < 2:
                 raise ValueError("No se han encontrado picos en el análisis por distanciamiento.")
 
@@ -147,13 +189,10 @@ class BloodPressure:
 
             # Rangos extremos 240>sys>70 & 140>dia>40
             sys, dia = int(pressures[idx_sys]), int(pressures[idx_dia])
-            if not(240>sys>70) or not(140>dia>40):
-                raise ValueError("Cálculo erroneo, presiones fuera de rangos factibles.")
+            if not (240 > sys > 70) or not (140 > dia > 40):
+                raise ValueError("Cálculo erróneo, presiones fuera de rangos factibles.")
             print(f"SYS: {sys} mmHg  |  DIA: {dia} mmHg")
             return sys, dia
 
         except ValueError as e:
             print(f"ValueError: {e}")
-
-
-
